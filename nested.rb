d3984e5e8414366a071b429bf6e7d19e0f443f0d
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-26 04:24:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,6 +118,7 @@ def changing_alan
      end
 
 def adding_to_dennis
+	value = "Assembly"
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -133,6 +134,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]<< <<  "Assembly"
+     programmer_hash[:dennis_ritchie][:languages]<<
      return programmer_hash
 end
