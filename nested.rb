a03052693134c6e142764d3ca7bf341af124a45c
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-26 04:02:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,9 +110,9 @@ def changing_alan
      
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     puts programmer_hash[:alan_kay][:known_for]
-     return programmer_hash[:alan_kay][:known_for]
-end
+     puts programmer_hash
+     return programmer_hash
+     end
 
 def adding_to_dennis
 	programmer_hash = 
